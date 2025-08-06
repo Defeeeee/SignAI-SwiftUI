9e588acbb2b1a03c0c80ab83c15617f82cd06f68
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2025-08-06 20:03:25 -0300

    Initial Commit

---
+++ SignAI-SwiftUI/SignAI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignAI_SwiftUIApp.swift
+//  SignAI-SwiftUI
+//
+//  Created by Federico Diaz Nemeth on 06/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SignAI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
