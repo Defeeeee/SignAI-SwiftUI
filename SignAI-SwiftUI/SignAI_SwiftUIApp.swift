cb2bfe116535ef274a5deedabc9036fc7d1f8a2c
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2025-08-17 21:10:20 -0300

    rocketsim

--- SignAI-SwiftUI/SignAI_SwiftUIApp.swift
+++ SignAI-SwiftUI/SignAI_SwiftUIApp.swift
@@ -5,10 +5,25 @@
 //  Created by Federico Diaz Nemeth on 06/08/2025.
 //
 
+
 import SwiftUI
+
+private func loadRocketSimConnect() {
+    #if DEBUG
+    guard (Bundle(path: "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework")?.load() == true) else {
+        print("Failed to load linker framework")
+        return
+    }
+    print("RocketSim Connect successfully linked")
+    #endif
+}
 
 @main
 struct SignAI_SwiftUIApp: App {
+    init() {
+        loadRocketSimConnect()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainView()
